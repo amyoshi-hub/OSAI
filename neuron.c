581b9ded9dddee313bd9afc3c048ce4a52a4052a
amyoshi <123788027+amyoshi-hub@users.noreply.github.com>
2025-05-13 20:43:47 +0900

    Add files via upload

--- neuron.c
+++ neuron.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <time.h>
 
-#define NMAX 6 // 列数
-#define MAX 16  // 行数
-
-double node[MAX][NMAX];
-double o_node[MAX][NMAX];
-double weight[MAX][NMAX];
-double del;
+#define MAX 4
+#define EPOCHS 1
+#define POS 0.2
+#define NEG 0.1
+#define ALPHA 0.1
 
 int inputs[4][2] = {
 	{0, 0},
@@ -17,84 +17,127 @@ int inputs[4][2] = {
 	{1, 1}
 };
 
-int targets[4] = {0, 1, 1, 0};
+int targets[4] = {0, 0, 0, 1};
 
-double rnd(){
-	srand(time(NULL));
-	return ((rand() % 1000) / 10.0);
-}
 
-void clear() {
-    printf("\033[2J");
-}
+void print_matrix(int rows, int cols, double *matrix, const char* label) {
+	printf("%s:\n", label);                                                                      for (int i = 0; i < rows; i++) {                                                                 	for (int j = 0; j < cols; j++) {                                                     		printf("%4f ", (float)matrix[i * cols + j]);
+	      }
+	printf("\n");
+	}                                                                                            printf("\n");                                                                            } 
 
-void print_matrix(int rows, int cols, double matrix[rows][cols], const char* label) {
-    printf("%s:\n", label);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%4f ", (float)matrix[i][j]); // 各値を4文字幅で整列
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
 
-void calc(double node[MAX][NMAX], double weight[MAX][NMAX], int loop){
-	for(int i = 0; i <= NMAX; i++){
-		for(int j = 0; j <= MAX; j++){
-			//o_nodeを二層目のa11から数えていけばいい
-			//基本は[j][i]単体だが周りの２つにも誤差を伝える
-			double del = weight[j][loop] * node[j][loop] + weight[j + 1][loop] * node[j + 1][loop];
-			del = abs(del);
-			printf("del>%lf\n", del);
-			//if(0.3 > del){
-				o_node[j][i] = del;
-				o_node[j - 1][i] += del;
-				o_node[j + 1][i] += del;
-			//}
-			
+void init(double **input, double **weight, double **out, int size){
+	*weight = (double *)malloc(sizeof(double) * size);
+	if(*weight == NULL){
+		perror("weight malloc faild\n");	
+		exit(1);
+	}
+	*input = (double *)malloc(sizeof(double) * size);
+	if(*input == NULL){
+		perror("weight malloc faild\n");	
+		exit(1);
+	}
+	*out = (double *)malloc(sizeof(double) * size / 2);
+	if(*out == NULL){
+		perror("weight malloc faild\n");	
+		exit(1);
+	}
+	for(int j = 0; j <= size; j++){
+			if(((j + 1) % 2) == 0){
+				(*input)[j] = -1;	
+			}else{
+				(*input)[j] = 1;
+			}	
+	}
+	double *temp = (double *)realloc(*weight, sizeof(double) * size);	
+	if(temp == NULL){
+		perror("realloc faild");	
+		exit(1);
+	}
+	*weight = temp;
+	for(int i = 0; i <= size; i++){
+	//delによるculc
+		if(((i) % 2) == 0){
+			//pos
+
+			(*weight)[i] = POS;
+			//(*weight)[i] = rnd();
+		}else{
+			//neg
+			(*weight)[i] = NEG;
 		}
 	}
 
 }
 
+double rnd(){
+	srand(time(NULL));
+	return ((rand() % 1000) / 1000.0);
+}
 
+//frontに値を回すときのどうしたら学習するか、隣のoutにも値を渡して誤差を共有してXORを学習させる
 
-int main() {
-    clear();
-    
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < NMAX; j++) {
-            if ((i % 2) == 0)
-                node[i][j] = 1;
-            else
-                node[i][j] = -1;
-        }
-    }
+//hebbian w[i] = ALPHA * input[i] * output[i];
+void update(double **weight, double *input, double *error, int size){
+	for(int i = 0; i < size; i++){
+		(*weight)[i] += ALPHA * error[i] * input[i];	
+	}	
 
-    //ここで上がポジティブと仲良しここで拮抗させると０ポジティブに対して大きくすれば出力はプラス
-    //に入力の判定はできたということだろうか？
-    for (int i = 0; i < MAX; i++) {
-        for (int j = 0; j < NMAX; j++) {
-		if((i % 2) == 0){
-			weight[i][j] = 2;
-		}else{
-			weight[i][j] = 1;	
-		}
-        }
-    }
-    int loop = 0;
+}
 
-    for(int i = 0; i < NMAX; i++){
-    	calc(node, weight, loop);
-	loop++;
-    }
+void culc(double **input, double **weight, double **out, int size){
+	double ans = 0;
+	int j = 0;
+	for(int i = 0; i < (size / 2); i++){
+		ans = (*input)[2 * i] * (*weight)[2 * i] + (*input)[2 * i + 1] * (*weight)[2 * i + 1];	
 
-    print_matrix(MAX, NMAX, node, "Node Matrix");
-    print_matrix(MAX, NMAX, weight, "Weight Matrix");
-    print_matrix(MAX, NMAX, o_node, "out node");
+		(*out)[i] = fmod(ans, (double)MAX);
 
+	}
+}
 
-    //printf("%lf", o_node[MAX/2][NMAX/2]);
-    return 0;
+int main(){
+	int e = 1;
+	//最大は2^27乗まで２８からはプロセス殺される
+	//134217728
+	for(int i = 0; i < 2; i++){
+		printf("%d\n", e *=2);	
+	}
+	int size = MAX;
+	int n = MAX;
+	int NMAX = 1;
+	while(n != 1){
+		n = n / 2;	
+		NMAX++;
+	}
+	double *weight;
+	double *input;
+	double *out;
+	double ans;
+	double error;
+
+	init(&input, &weight, &out, size);
+	//print_matrix(size, 1, input, "input");
+
+	sina_c(&weight, size, 0);
+	for(int i = 0; i < NMAX; i++){
+		for()	
+		culc(&input,&weight,&out,size);
+		memcpy(input, out, sizeof(double) * size / 2);
+		input = (double *)realloc(input, sizeof(double) * size);	
+		out = (double *)realloc(out, sizeof(double) * size / 2);	
+		error = target[i] - out;
+		update(&weight, size, )
+		//print_matrix(size / 2, 1, input, "input");
+		print_matrix(size / 2, 1, out, "out");
+		print_matrix(size / 2, 1, weight, "weight");
+		size = size / 2;
+	}
+	print_matrix(1, 1, out, "out");
+	free(input);
+	free(weight);
+	free(out);
+
+	return 0;
 }
