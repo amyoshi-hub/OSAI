5757b99fca15eb047de030f079a9f2069fdd1083
amyoshi <123788027+amyoshi-hub@users.noreply.github.com>
2025-05-17 22:53:09 +0900

    Create get_image_pixel.cs

---
+++ rendering/get_image_pixel.cs
@@ -0,0 +1,40 @@
+﻿using SkiaSharp;
+
+class Program
+{
+	static void Main(){
+		int width = 800, height = 600;	
+		using var surface = SKSurface.Create(new SKImageInfo(width, height));
+		var canvas = surface.Canvas;
+		canvas.Clear(SKColor.Parse("#003366"));
+		using var bitmap = SKBitmap.Decode("OSAI.png");
+
+		for(var y = 0; y < bitmap.Height; y++){
+			for(var x = 0; x < bitmap.Width; x++){
+				var pixelColor = bitmap.GetPixel(x, y);	
+				System.Console.WriteLine(pixelColor);
+
+				//SKColor has ４ parameter may be RGB?
+				var modColor = new SKColor(
+					red: (byte)(x % 255),			
+					green: (byte)(y % 255),
+					blue: (byte)(x + y % 255),
+					alpha: pixelColor.Alpha);
+
+				var rectPaint = new SKPaint {Color = modColor};
+				canvas.DrawRect(x, y, 1, 1, rectPaint);
+			}
+		}
+		using var image = surface.Snapshot();
+		using var data = image.Encode(SKEncodedImageFormat.Png, 100);
+
+		const string outputPath = "out.png";
+		using(var stream = File.OpenWrite(outputPath))
+		{
+			data.SaveTo(stream);
+		}
+
+		System.Console.WriteLine($"saved image:{outputPath}");
+	}
+	
+}
