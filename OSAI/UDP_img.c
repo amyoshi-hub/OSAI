193cfe20f4b279ed7cef6065eaec2728d45335e6
amyoshi <123788027+amyoshi-hub@users.noreply.github.com>
2025-06-01 20:39:51 +0900

    Add files via upload

--- OSAI/UDP_img.c
+++ OSAI/UDP_img.c
@@ -26,6 +26,86 @@ static void build_ip(char *p, struct in_addr *src, struct in_addr *dst, size_t l
 static void build_udp(char *p, struct in_addr *src, struct in_addr *dst, unsigned short dport, uint32_t chunk, char *data, long datasize);
 static char *img_read(const char *filepath, long *filesize);
 
+static unsigned short in_cksum(unsigned short *addr, int len) {
+    int sum = 0;
+    unsigned short result;
+
+    while (len > 1) {
+        sum += *addr++;
+        len -= 2;
+    }
+
+    if (len == 1) {
+        sum += *(unsigned char *)addr;
+    }
+
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    result = ~sum;
+    return result;
+}
+
+static void build_ip(char *p, struct in_addr *src, struct in_addr *dst, size_t len) {
+    struct ip *ip_hdr = (struct ip *)p;
+    ip_hdr->ip_hl = 5;
+    ip_hdr->ip_v = 4;
+    ip_hdr->ip_tos = 0;
+    ip_hdr->ip_len = htons(len);
+    ip_hdr->ip_id = 0;
+    ip_hdr->ip_off = 0;
+    ip_hdr->ip_ttl = 64;
+    ip_hdr->ip_p = IPPROTO_UDP;
+    ip_hdr->ip_sum = 0;
+    ip_hdr->ip_src = *src;
+    ip_hdr->ip_dst = *dst;
+    ip_hdr->ip_sum = in_cksum((unsigned short *)ip_hdr, sizeof(struct ip));
+}
+static void build_udp(char *p, struct in_addr *src, struct in_addr *dst, unsigned short dport, uint32_t chunk, char *data, long datasize) {
+    struct udphdr *udp_hdr = (struct udphdr *)(p + sizeof(struct ip));
+    struct pseudo_hdr phdr = { *src, *dst, 0, IPPROTO_UDP, htons(sizeof(struct udphdr) + datasize + 4) };
+
+    udp_hdr->uh_sport = htons(12345); // 任意の送信元ポート番号
+    udp_hdr->uh_dport = htons(dport);
+    udp_hdr->uh_ulen = htons(sizeof(struct udphdr) + datasize + 4);
+    udp_hdr->uh_sum = 0;
+
+    memcpy((char *)(udp_hdr + 1), &chunk, 4);
+    if (datasize > 0) {
+        memcpy((char *)(udp_hdr + 1) + 4, data, datasize);
+    }
+
+    udp_hdr->uh_sum = in_cksum((unsigned short *)&phdr, sizeof(phdr));
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static char *img_read(const char *filepath, long *filesize) {
+    FILE *file = fopen(filepath, "rb");
+    if (!file) {
+        perror("fopen");
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    *filesize = ftell(file);
+    rewind(file);
+
+    char *data = malloc(*filesize);
+    if (!data) {
+        perror("malloc");
+        fclose(file);
+        return NULL;
+    }
+
+    fread(data, 1, *filesize, file);
+    fclose(file);
+
+    return data;
+}
+
+
+
 int send_image_over_udp(const char *src_ip, const char *dst_ip, unsigned short port, const char *imgfile) {
     int sd;
     int on = 1;
